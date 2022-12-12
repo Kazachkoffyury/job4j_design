44bfcdec8d09cf9e22d56c3224fc4ae8c84c0d1a
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-12-12 23:24:08 +0300

    addModelBoxFix#1

--- src/test/java/ru/job4j/BoxTest.java
+++ src/test/java/ru/job4j/BoxTest.java
@@ -15,49 +15,49 @@ class BoxTest {
 
     @Test
     void isThisTetrahedron() {
-        Box box = new Box(4,5);
+        Box box = new Box(4, 5);
         String name = box.whatsThis();
         assertThat(name).isEqualTo("Tetrahedron");
     }
 
     @Test
     void isThisCube() {
-        Box box = new Box(8,5);
+        Box box = new Box(8, 5);
         String name = box.whatsThis();
         assertThat(name).isEqualTo("Cube");
     }
 
     @Test
     void isThisUnknownObject() {
-        Box box = new Box(10,5);
+        Box box = new Box(10, 5);
         String name = box.whatsThis();
         assertThat(name).isEqualTo("Unknown object");
     }
 
     @Test
     void getNumberVerticesUnknownObjectTest() {
-        Box box = new Box(3,5);
+        Box box = new Box(3, 5);
         int number = box.getNumberOfVertices();
         assertThat(number).isEqualTo(-1);
     }
 
     @Test
     void getNumberVerticesUnknownTest() {
-        Box box = new Box(4,5);
+        Box box = new Box(4, 5);
         int number = box.getNumberOfVertices();
         assertThat(number).isEqualTo(4);
     }
 
     @Test
     void isExistTest() {
-        Box box = new Box(4,5);
+        Box box = new Box(4, 5);
         boolean isExist = box.isExist();
         assertThat(isExist).isTrue();
     }
 
     @Test
-    void getAreaTest(){
-       Box box = new Box(8,2);
+    void getAreaTest() {
+       Box box = new Box(8, 2);
        double area = box.getArea();
        assertThat(area).isEqualTo(24);
     }

61e4f62f8f5223616ef2d442d45b0bd11005e285
Kazackoffyury <kazachkoff.yury@yandex.ru>
2023-01-17 23:56:07 +0300

    SimpleLinkedList

---
+++ src/main/java/ru/job4j/simplelinkedlist/SimpleLinkedList.java
@@ -0,0 +1,92 @@
+package ru.job4j.simplelinkedlist;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+
+public SimpleLinkedList() {
+
+}
+
+    @Override
+    public void add(E value) {
+
+
+      if (size == 0) {
+          head = new Node<>(value);
+      } else {
+          getLastNode(size - 1).next = new Node<>(value);
+
+      }
+      size++;
+      modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    private Node<E> getLastNode(int index) {
+        Node<E> nodeTemp = this.head;
+        if (Objects.checkIndex(index, size) == index) {
+            for (int i = 0; i < index; i++) {
+                nodeTemp = nodeTemp.next;
+            }
+        }
+        return nodeTemp;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            final int expectedModCount = modCount;
+            Node<E> currentNode = head;
+            private E result;
+
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return currentNode != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                result = currentNode.item;
+                currentNode = currentNode.next;
+                return result;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+
+        Node(E element) {
+            this.item = element;
+
+
+        }
+    }
+}
+
